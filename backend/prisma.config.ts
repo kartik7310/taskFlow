2da80c7791fe99a0593e7e029ac3d8ecd9b32658
kartik lathiyan <kartiklathiyan455@gmail.com>
2026-03-16 17:40:44 +0530

    fix some url db url

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -2,13 +2,13 @@
 // npm install --save-dev prisma dotenv
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
-
+import env from "./src/config/env.js";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"]!,
+    url: env.DATABASE_URL,
   },
 });
